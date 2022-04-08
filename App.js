8949740fbddf331d10c2f80eca87ba0f17931eff
Long Nguyễn <thanhlong090101@gmail.com>
2022-04-08 16:54:01 +0700

    tab

--- App.js
+++ App.js
@@ -1,12 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Device from './Screens/Device';
 import Main from './Screens/Main';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { NavigationContainer } from '@react-navigation/native';
+// import { useState } from 'react';
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
+  // const [tab, setTab] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Main/>
-      {/* <Device/> */}
+      <NavigationContainer>
+        <Tab.Navigator 
+          screenOptions={{
+            // tabBarActiveBackgroundColor: 'red',
+            tabBarStyle: {backgroundColor: '#343434',},
+          }}
+          sceneContainerStyle={{backgroundColor: '#343434'}}
+        >
+          <Tab.Screen 
+            screenOptions={{tabBarStyle: {backgroundColor: 'red'}}}
+            name='Main' 
+            component={Main} 
+            options={{
+              title: () => null,
+              header: () => null, 
+              tabBarIcon: ({ focused }) => (
+                <Ionicons name="md-apps-outline" size={24} color="white" style={focused ? styles.nav: null}/>
+              ),
+            }}
+          />
+          <Tab.Screen 
+            screenOptions={{
+              tabBarStyle: {backgroundColor: '#343434',}}} 
+            name='Device' 
+            component={ Device } 
+            options={{
+              title: () => null,
+              header: () => null, 
+              tabBarIcon: ({ focused }) => (
+                <MaterialIcons name="device-hub" size={24} color="white" style={focused ? styles.nav: null}/>
+              ),
+            }}
+            
+          />
+          <Tab.Screen 
+            screenOptions={{
+              tabBarStyle: {backgroundColor: '#343434',}}} 
+            name='Chart' 
+            component={Main} 
+            options={{
+              title: () => null,
+              header: () => null, 
+              tabBarIcon: ({ focused }) => (
+                <Ionicons name="bar-chart-outline" size={24} color="white" style={focused ? styles.nav: null}/>
+              )}}
+          />
+          <Tab.Screen 
+            screenOptions={{
+              tabBarStyle: {backgroundColor: '#343434',}}} 
+            name='Notifications'  
+            component={Main} 
+            options={{
+              title: () => null,
+              header: () => null, 
+              tabBarIcon: ({ focused }) => (
+                <Ionicons name="ios-notifications-outline" size={24} color='white' style={focused ? styles.nav: null}/>
+              ),
+            }}
+          />
+
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,10 +84,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#343434',
-    backgroundColor: 'white',
+    backgroundColor: '#343434',
+    // backgroundColor: 'white',
     // alignItems: 'center',
     // justifyContent: 'center',
     color: 'white',
   },
+  nav: {
+    borderRadius: 16, 
+    backgroundColor: '#E335DC',
+    padding: 9,
+  }
 });
--- Screens/Main.js
+++ Screens/Main.js
@@ -4,47 +4,67 @@ import styled from 'styled-components';
 // import { Shadow } from 'react-native-shadow-2';
 import { LinearGradient } from "expo-linear-gradient";
 import AppLoading from 'expo-app-loading';
-import { useFonts,Lato_700Bold } from '@expo-google-fonts/lato';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import {
+    useFonts,
+    Lato_100Thin,
+    Lato_100Thin_Italic,
+    Lato_300Light,
+    Lato_300Light_Italic,
+    Lato_400Regular,
+    Lato_400Regular_Italic,
+    Lato_700Bold,
+    Lato_700Bold_Italic,
+    Lato_900Black,
+    Lato_900Black_Italic,
+  } from '@expo-google-fonts/lato';
 
 export default function Main() {
     let [fontsLoaded] = useFonts({
+        Lato_100Thin,
+        Lato_100Thin_Italic,
+        Lato_300Light,
+        Lato_300Light_Italic,
+        Lato_400Regular,
+        Lato_400Regular_Italic,
         Lato_700Bold,
-    });
+        Lato_700Bold_Italic,
+        Lato_900Black,
+        Lato_900Black_Italic,
+      });
     if (!fontsLoaded) {
         return <AppLoading />;
     } else {
         return (
             <View style={{flex: 1, flexDirection: 'column',}}>
-                <View style={{flex: 2,}}>
+                <View style={{flex: 1.2,}}>
                     <Title >My Data</Title>
                 </View>   
                 <Container>
                     <View style={{flex: 1, flexDirection: 'row',}}>
                         <View  style={{ flex: 1, }}> 
-                            <ContainerManual elevation={11} style={{shadowColor: "#000000",
-shadowOffset: {
-	width: 0,
-	height: 7,
-},
-shadowOpacity: 0.43,
-shadowRadius: 9.51,}}>
-                                {/* <LinearGradient
+                            <ContainerManual>
+                                <LinearGradient
                                     colors={["#E335DC", "#F404E9"]}
                                     style={styles.buttonContainer}
                                     end={{ x: 0.1, y: 0.2 }}
-                                >    */}
-                                <Key>Long</Key>                    
-                                {/* </LinearGradient>   */}
+                                >   
+                                <Key>Temperature</Key>     
+                                <Value>
+                                    30 
+                                    <MaterialCommunityIcons name="temperature-celsius" size={50} color="white" />
+                                </Value>               
+                                </LinearGradient>  
                             </ContainerManual>
                         </View>
                         <View style={{ flex: 1, }} > 
                             <ContainerManual>
                                 <LinearGradient
-                                    colors={["#9142FF", "#710CFF"]}
+                                    colors={["#9142FF", "#A264FA" , "#B992F0"]}
                                     style={styles.buttonContainer}
                                 >   
-                                <Text>Long</Text>                    
+                                <Key>Humidity</Key>
+                                <Value>83 %</Value>                    
                                 </LinearGradient>  
                             </ContainerManual>
                         </View>
@@ -56,7 +76,8 @@ shadowRadius: 9.51,}}>
                                     colors={["#00D092", "#00B17C" ]}
                                     style={styles.buttonContainer}
                                 >   
-                                <Text>Long</Text>                    
+                                <Key>Light</Key>
+                                <Value>On</Value>                    
                                 </LinearGradient>  
                             </ContainerManual>
                         </View>
@@ -66,15 +87,67 @@ shadowRadius: 9.51,}}>
                                     colors={["#4B4848", "#010101"]}
                                     style={styles.buttonContainer}
                                 >   
-                                <Text>Long</Text>                    
+                                <Key>Active</Key>  
+                                <ActiveValue>Fan</ActiveValue>                  
+                                <ActiveValue>Pump</ActiveValue>                  
+                                <ActiveValue>Light</ActiveValue>                  
                                 </LinearGradient>  
                             </ContainerManual>
                         </View>
                     </View>
                 </Container>
-                <View style={{flex: 4,}}>
-                    <Text>Long</Text>
-                </View>
+                <ContainerDevices style={{flex: 2.5, flexDirection: 'column',}}>
+                    <View style={{flex: 1, }}>
+                        <TitleDevices>Devices</TitleDevices>
+                    </View>
+                    <View style={{flex: 4, flexDirection: 'column', paddingTop: 15,}}>
+                        <View style={{flex: 1, flexDirection: 'row'}}>
+                            <View style={{flex: 1, }}>
+                                <ContainerButton>
+                                    <LinearGradient
+                                        colors={["#9142FF", "#710CFF"]}
+                                        style={styles.buttonCouter}
+                                    >   
+                                    <ButtonDevices>Fan</ButtonDevices>                  
+                                    </LinearGradient>  
+                                </ContainerButton>
+                            </View>
+                            <View style={{flex: 1, }}>
+                                <ContainerButton>
+                                    <LinearGradient
+                                        colors={["#00D092", "#72AC9A"]}
+                                        style={styles.buttonCouter}
+                                    >   
+                                    <ButtonDevices>Pump</ButtonDevices>                  
+                                    </LinearGradient>  
+                                </ContainerButton>
+                            </View>
+                        </View>
+                        <View style={{flex: 1, flexDirection: 'row'}}>
+                            <View style={{flex: 1, }}>
+                                <ContainerButton>
+                                    <LinearGradient
+                                        colors={["#E335DC", "#DBADD9"]}
+                                        style={styles.buttonCouter}
+                                    >   
+                                    <ButtonDevices>Light</ButtonDevices>                  
+                                    </LinearGradient>  
+                                </ContainerButton>
+                            </View>
+                            <View style={{flex: 1, }}>
+                                <ContainerButton>
+                                    <LinearGradient
+                                        colors={["#D44444", "#F20000"]}
+                                        style={styles.buttonCouter}
+                                    >   
+                                    <ButtonDevices>Back</ButtonDevices>                  
+                                    </LinearGradient>  
+                                </ContainerButton>
+                            </View>
+                        </View>
+                        
+                    </View>
+                </ContainerDevices>
             </View>
         );
     }
@@ -91,11 +164,11 @@ const styles = StyleSheet.create({
       padding: 15, 
       alignItems: "flex-start", 
       borderRadius: 20,
-      width: '100%', height: '100%', 
+      width: '100%', height: '98%', 
     },
     buttonCouter: { 
-      padding: 15, 
-      alignItems: "flex-start", 
+      padding: 17, 
+      alignItems: "center", 
       borderRadius: 20,
     },
     buttonText: {
@@ -110,7 +183,7 @@ const Title = styled.Text`
     font-size: 30px;
     font-weight: 700;
     margin-left: 10px;
-    margin-top: 40px;
+    margin-top: 30px;
     margin-bottom: 20px;
     letter-spacing: 2px;
 `
@@ -131,11 +204,44 @@ const Key = styled.Text`
     color: white;
     padding: 0px 0px 0px 10px;
     letter-spacing: 2px;    
+    font-family: Lato_400Regular;
+    margin-top: 5px;
 `
 const Value = styled.Text`
+    margin-top: 15px;
     color: white;
     padding: 5px 0px 0px 10px;
     letter-spacing: 2px;
+    font-size: 50px;
+    font-family: Lato_700Bold;
+`
+const ActiveValue = styled.Text`
+    color: #00D092;
+    margin-top: 10px;
+    margin-left: 10px;
+`
+const TitleDevices = styled.Text`
+    color: white;
+    font-size: 30px;
+    font-weight: 700;
+    margin-left: 10px;
+    /* margin-top: 30px; */
+    /* margin-bottom: 4px; */
+    letter-spacing: 2px;
+`
+const ContainerDevices = styled.View`
+    /* background-color: red; */
+`
+const ContainerButton = styled.View`
+    border-radius: 20px;
+    margin-left: 5px;
+    margin-right: 5px;
+`
+const ButtonDevices = styled.Text`
+    color: white;
+    letter-spacing: 1.5px;    
+    font-family: Lato_700Bold;
+    font-weight: 600;
 `
 
 
